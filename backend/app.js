61b5f54907d4fc5f2e18feddbd2d9c9c60438d66
dhirajsuthar781 <146662607+dhirajsuthar781@users.noreply.github.com>
2024-02-22 23:13:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieparser());
 
 app.use(cors({
-    origin: "https://payday1.vercel.app",
+    origin: "https://pay-front-x0xq.onrender.com",
 }));
 
 connection();
